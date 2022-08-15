c85b5f9d5ba750e6a2588d3da01b0434eab10f3c
Pratul08 <pratuljohari92@gmail.com>
2022-08-16 02:55:13 +0530

    my 1 website

---
+++ a1.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Pratul Johari</title>
+	<link rel="stylesheet" type="text/css" href="a2.css">
+</head>
+<body>
+<div class="page">
+	<div class="heading">
+		<div class="insta">
+		<a href="https://www.instagram.com/pratul_johari/">
+         <img src="image/insta.jpg"
+         width="30" height="30">
+      </a>
+  </div>
+  <br>
+			<ul>
+				<li><a href="https://www.sandeepmaheshwari.com/contact.aspx">Contact</a></li>
+				<li><a href="https://www.sandeepmaheshwari.com/freedownload/">Free Downloads</a></li>
+				<li><a href="https://www.sandeepmaheshwari.com/myfavourites.aspx">Recommended Books</a></li>
+				<li><a href="https://www.youtube.com/channel/UCWbyF1fD8pZT-XqidrQndFQ">Videos</a></li>	
+				<li><a href="https://www.sandeepmaheshwari.com/thePerson.aspx">The Person</a></li>
+				<li><a href="http://localhost/prac/a1.php">Home</a></li>				
+			</ul>
+			<h1>Pratul Johari</h1>
+	</div>
+	<div class="pic"><img src="image/Project 2.jpg"></div>
+	<div class="main">
+		
+		<p>Pratul Johari is a name among millions who struggled, failed and surged ahead in search of success, happiness and contentment. Just like any middle class guy, he too had a bunch of unclear dreams and a blurred vision of his goals in life. All he had was an undying learning attitude to hold on to. Rowing through ups and downs, it was time that taught him the true meaning of his life.</p>
+		<br>	
+		<h2>How to attend a Live Session?</h2>
+		<br>
+		<p>Just register yourself by clicking on the button given below and we will let you know whenever there's a session in your city/state:</p>
+	
+	<div class="register" >
+		<input type="submit" value="  Register For Free >>>  " class="btn" name="register">
+	</div>
+</div>
+	<div class="car">
+		<div class="card">
+			<h3>Get Inspired</h3>
+			<br>
+			<p>To receive notifications of new videos, simply subscribe
+to Sandeep Maheshwari's YouTube channel.</p>
+		</div>
+		
+		<div class="card2">
+			<h3>Let's Connect</h3>
+			<br>
+			<p>To get regular updates on the forthcoming sessions,
+simply visit Sandeep Maheshwari's Facebook Page.</p>
+		</div>
+
+		<!-- <h3>Get Inspired</h3>
+		<br>
+		<p>To receive notifications of new videos, simply subscribe
+		to Sandeep Maheshwari's YouTube channel.</p>
+		</div>
+		<div class="card2">
+		<h3>Let's Connect</h3>
+		<br>
+		<p>To get regular updates on the forthcoming sessions,
+		simply visit Sandeep Maheshwari's Facebook Page.</p>
+		 --></div>
+	</div>
+
+
+</body>
+</html>
